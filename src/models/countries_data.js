e0e2d3fcf5b4bbea1c9622ba6507860ff8f8e450
Joe Stafford <joestafford1986@gmail.com>
2018-06-27 18:22:49 +0100

    initial commit, xhr setup

---
+++ src/models/countries_data.js
@@ -0,0 +1,14 @@
+const Request = require('../helpers/request.js');
+const PubSub = require('../helpers/pub_sub.js');
+
+const CountriesData = function() {
+
+};
+
+CountriesData.prototype.getAllData = function () {
+  const url = `https://restcountries.eu/rest/v2/all`;
+  request = new Request(url);
+  request.get((allCountriesData) => {
+    PubSub.publish('CountriesData:all-data-ready', allCountriesData);
+  });
+};
