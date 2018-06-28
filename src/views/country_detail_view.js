bec013fe608f2b232ac8f5888c8eaa93072c28b9
Joe Stafford <joestafford1986@gmail.com>
2018-06-28 09:37:40 +0100

    added languages but dont display

--- src/views/country_detail_view.js
+++ src/views/country_detail_view.js
@@ -18,18 +18,34 @@ CountryDetailView.prototype.render = function (countryData) {
 
   const countryName = country.name;
   const countryRegion = country.region;
-  console.log(country);
 
   const nameElement = this.createElement('h2', countryName);
   this.element.appendChild(nameElement);
   const regionElement = this.createElement('h3', `Region: ${countryRegion}`);
   this.element.appendChild(regionElement);
+  const languagesUnorderedListElement = this.createElement('ul', 'Languages:');
+  this.element.appendChild(languagesUnorderedListElement);
+
+  const countryLanguages = country.languages[0];
+
+  console.log(countryLanguages);
+
+  for (const language in countryLanguages) {
+    console.log(language);
+    const languageItem = this.createListItems(languagesUnorderedListElement, language);
+    languagesUnorderedListElement.appendChild(languageItem);
+  }
 };
 
 CountryDetailView.prototype.createElement = function (elementType, text) {
   const newElement = document.createElement(elementType);
   newElement.textContent = text;
   return newElement;
+};
+
+CountryDetailView.prototype.createListItems = function (list, item) {
+  const listElement = document.createElement('li', item);
+  return listElement;
 };
 
 module.exports = CountryDetailView;
